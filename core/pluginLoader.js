d25941bb3414d8e99138a51102a191e2ba7293ea
ONOFRIUS Core <onofrius@users.noreply.github.com>
2026-08-24 18:22:50 +0200

    sync: merge accumulated knowledge, style profiles and pluginLoader from Downloaded ONOFRIUS archive

--- core/pluginLoader.js
+++ core/pluginLoader.js
@@ -4,40 +4,34 @@ const pluginManager = require("./pluginManager");
 const logger = require("./logger");
 
 function loadPlugins() {
+    let pluginsDir = path.join(__dirname, "..", "plugins");
+    if (!fs.existsSync(pluginsDir)) {
+        pluginsDir = path.join(__dirname, "..", "..", "plugins");
+    }
 
-    const pluginsDir = path.join(__dirname, "..", "plugins");
+    if (!fs.existsSync(pluginsDir)) {
+        logger.warn("Plugin", `Directory plugin non trovata: ${pluginsDir}`);
+        return;
+    }
 
     const folders = fs.readdirSync(pluginsDir);
 
     for (const folder of folders) {
-
         const pluginPath = path.join(pluginsDir, folder, "index.js");
 
         if (!fs.existsSync(pluginPath))
             continue;
 
         try {
-
             const plugin = require(pluginPath);
-logger.debug(
-    "Plugin",
-    `Registrazione: ${plugin.name}`
-);
+            logger.debug("Plugin", `Registrazione: ${plugin.name}`);
             pluginManager.register(plugin);
-
         } catch (err) {
-            logger.error(
-                `Errore caricando ${folder}: ${err.message}`
-            );
-
+            logger.error(`Errore caricando ${folder}: ${err.message}`);
         }
-
     }
-
 }
 
 module.exports = {
-
     loadPlugins
-
 };
